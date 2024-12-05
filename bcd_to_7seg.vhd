578c9dba413d5c9a8f64dd7f7656ccc3c8d7b2a4
amkdcabanek <163867301+amkdcabanek@users.noreply.github.com>
2024-12-05 20:35:20 +0100

    Update bcd_to_7seg.vhd

--- bcd_to_7seg.vhd
+++ bcd_to_7seg.vhd
@@ -9,15 +9,15 @@ end bcd_to_7segment;
 architecture Behavioral of bcd_to_7segment is
 begin
 
-process(BCDin)
+process(BCDin)  --converts a 4-bit Binary-Coded Decimal (BCD) input (BCDin) to a 7-segment display output
 begin
     case BCDin is
         when "0000" =>
-            Seven_Segment <= "0000001";
+            Seven_Segment <= "0000001"; --0
         when "0001" =>
-            Seven_Segment <= "1001111";
+            Seven_Segment <= "1001111"; --1
         when "0010" =>
-            Seven_Segment <= "0010010";
+            Seven_Segment <= "0010010"; --2
         when "0011" =>
             Seven_Segment <= "0000110";
         when "0100" =>
@@ -31,9 +31,9 @@ begin
         when "1000" =>
             Seven_Segment <= "0000000";
         when "1001" =>
-            Seven_Segment <= "0000100";
+            Seven_Segment <= "0000100"; --9
         when others =>
-            Seven_Segment <= "1111111";
+            Seven_Segment <= "1111111"; --wylaczane
     end case;
 end process;
 
